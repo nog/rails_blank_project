8173f96b78f5df52e0a670aaec20ac0b4ad94c33
Shingo Noguchi <nog0926@gmail.com>
2019-02-25 23:35:22 +0900

    rails _4.0.13_ new . --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBlankProject::Application.config.secret_key_base = '84a3c19b7dcb605ddb071f4f7b7a21e92828a777971bae1be8a4163d05f39c787cdc5ae5105c9998718bb28cb282f0b2ce37415644500766cb4c2c0308440c8f'
